27920aacaafc5561d9185ed7903cd3cc44625ac2
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-02-12 17:56:50 +0100

    chore: Reformat

--- passfail2.py
+++ passfail2.py
@@ -23,9 +23,11 @@ except ImportError:
     try:
         from anki.utils import pointVersion as point_version
     except:
-        from anki import version # pyright: ignore
+        from anki import version  # pyright: ignore
+
         def point_version():
             return int(version.split(".")[-1])
+
 
 from anki.hooks import wrap
 
@@ -43,54 +45,56 @@ elif point_version() >= 36:
 else:
     from anki.lang import _
 
+
 # Hooks
 def pf2_hook_replace_buttons(
-        buttons_tuple, # type: tuple[tuple[int, str], ...]
-        reviewer,      # type: Reviewer
-        card           # type: Card
-): # type: (...) -> tuple[tuple[int,str], ...]
-    return (
-        (1, "Fail"),
-        (reviewer._defaultEase(), "Pass")
-    )
+    buttons_tuple,  # type: tuple[tuple[int, str], ...]
+    reviewer,  # type: Reviewer
+    card,  # type: Card
+):  # type: (...) -> tuple[tuple[int,str], ...]
+    return ((1, "Fail"), (reviewer._defaultEase(), "Pass"))
+
 
 def pf2_hook_remap_answer_ease(
-        ease_tuple, # type: tuple[bool, Literal[1, 2, 3, 4]]
-        reviewer,   # type: Reviewer
-        card        # type: Card
-): # type: (...) -> tuple[bool, Literal[1, 2, 3, 4]]
+    ease_tuple,  # type: tuple[bool, Literal[1, 2, 3, 4]]
+    reviewer,  # type: Reviewer
+    card,  # type: Card
+):  # type: (...) -> tuple[bool, Literal[1, 2, 3, 4]]
     (cont, ease) = ease_tuple
     if ease == 1:
         return ease_tuple
     else:
         return (cont, reviewer._defaultEase())
 
+
 # Shims for old versions of anki
 def pf2_shim_answerButtonList(
-        self, # type: Reviewer
-        _old  # type: Callable[[Reviewer], tuple[tuple[int, str], ...]]
-): # type: (...) -> tuple[tuple[int, str], ...]
+    self,  # type: Reviewer
+    _old,  # type: Callable[[Reviewer], tuple[tuple[int, str], ...]]
+):  # type: (...) -> tuple[tuple[int, str], ...]
     result = _old(self)
     if self.card:
         return pf2_hook_replace_buttons(result, self, self.card)
     else:
         return result
 
+
 def pf2_shim_answerCard(
-        self, # type: Reviewer
-        ease, # type: Literal[1, 2, 3, 4]
-        _old  # type: Callable[[Reviewer, Literal[1, 2, 3, 4]], None]
-): # type: (...) -> None
-    if self.card: # Should always be true
+    self,  # type: Reviewer
+    ease,  # type: Literal[1, 2, 3, 4]
+    _old,  # type: Callable[[Reviewer, Literal[1, 2, 3, 4]], None]
+):  # type: (...) -> None
+    if self.card:  # Should always be true
         (_, new_ease) = pf2_hook_remap_answer_ease((True, ease), self, self.card)
         return _old(self, new_ease)
     else:
         return _old(self, ease)
 
+
 # Run after drawing ease buttons
 def pf2_fix_pass_title(
-        self # type: Reviewer
-): # type: (...) -> None
+    self,  # type: Reviewer
+):  # type: (...) -> None
     title = None
     if point_version() >= 45:
         title = tr.actions_shortcut_key(val=2)
@@ -99,9 +103,8 @@ def pf2_fix_pass_title(
     else:
         title = _("Shortcut key: %s") % 2
 
-    self.bottom.web.eval(
-        f'document.getElementById("defease").title = "{title}";'
-    )
+    self.bottom.web.eval(f'document.getElementById("defease").title = "{title}";')
+
 
 # Init
 def init():
@@ -111,13 +114,17 @@ def init():
     if version >= 31:
         gui_hooks.reviewer_will_init_answer_buttons.append(pf2_hook_replace_buttons)
     else:
-        Reviewer._answerButtonList = wrap(Reviewer._answerButtonList, pf2_shim_answerButtonList, 'around')
+        Reviewer._answerButtonList = wrap(
+            Reviewer._answerButtonList, pf2_shim_answerButtonList, "around"
+        )
 
     # Remap ease for keybinds
     if version >= 20:
         gui_hooks.reviewer_will_answer_card.append(pf2_hook_remap_answer_ease)
     else:
-        Reviewer._answerCard = wrap(Reviewer._answerCard, pf2_shim_answerCard, 'around')
+        Reviewer._answerCard = wrap(Reviewer._answerCard, pf2_shim_answerCard, "around")
 
     # Show "Shortcut key: 2" rather than "Shortcut key: 3" for "Pass" button
-    Reviewer._showEaseButtons = wrap(Reviewer._showEaseButtons, pf2_fix_pass_title, 'after')
+    Reviewer._showEaseButtons = wrap(
+        Reviewer._showEaseButtons, pf2_fix_pass_title, "after"
+    )
